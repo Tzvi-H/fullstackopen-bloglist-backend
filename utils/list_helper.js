49ecab8e0fad341e9d6e4a218ad9a9b0f2c47dcd
Tzvi-H <16guitar@gmail.com>
2022-04-04 14:38:15 -0500

    4.7 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,4 +1,4 @@
-const dummy = blogs => {
+const dummy = () => {
   return 1
 }
 
@@ -25,7 +25,7 @@ const favoriteBlog = blogs => {
 const mostBlogs = blogs => {
   if (blogs.length === 0) return {};
 
-  const frequency = { [blogs[0].author]: 1 }
+  const blogFrequency = { [blogs[0].author]: 1 }
 
   let authorWithMostBlogs = {
     author: blogs[0].author,
@@ -33,12 +33,12 @@ const mostBlogs = blogs => {
   }
 
   blogs.slice(1).forEach(blog => {
-    frequency[blog.author] = frequency[blog.author] || 0;
-    frequency[blog.author]++;
-    if (frequency[blog.author] > authorWithMostBlogs.blogs) {
+    blogFrequency[blog.author] = blogFrequency[blog.author] || 0;
+    blogFrequency[blog.author]++;
+    if (blogFrequency[blog.author] > authorWithMostBlogs.blogs) {
       authorWithMostBlogs = {
         author: blog.author,
-        blogs: frequency[blog.author]
+        blogs: blogFrequency[blog.author]
       }
     }
   })
@@ -46,11 +46,36 @@ const mostBlogs = blogs => {
   return authorWithMostBlogs;
 }
 
+const mostLikes = blogs => {
+  if (blogs.length === 0) return {};
+
+  const likeFrequency = { [blogs[0].author]: blogs[0].likes }
+
+  let authorWithMostLikes = {
+    author: blogs[0].author,
+    likes: blogs[0].likes,
+  }
+
+  blogs.slice(1).forEach(blog => {
+    likeFrequency[blog.author] = likeFrequency[blog.author] || 0;
+    likeFrequency[blog.author]++;
+    if (likeFrequency[blog.author] > authorWithMostLikes.likes) {
+      authorWithMostLikes = {
+        author: blog.author,
+        likes: likeFrequency[blog.author]
+      }
+    }
+  })
+
+  return authorWithMostLikes;
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
-  mostBlogs
+  mostBlogs,
+  mostLikes
 }
 
 console.log(mostBlogs([]))
