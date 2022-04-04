d680dce419bd9d6cc2ccf42ea1fb6ead0ab1e366
Tzvi-H <16guitar@gmail.com>
2022-04-04 13:40:00 -0500

    4.6 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -22,8 +22,35 @@ const favoriteBlog = blogs => {
   }
 }
 
+const mostBlogs = blogs => {
+  if (blogs.length === 0) return {};
+
+  const frequency = { [blogs[0].author]: 1 }
+
+  let authorWithMostBlogs = {
+    author: blogs[0].author,
+    blogs: 1,
+  }
+
+  blogs.slice(1).forEach(blog => {
+    frequency[blog.author] = frequency[blog.author] || 0;
+    frequency[blog.author]++;
+    if (frequency[blog.author] > authorWithMostBlogs.blogs) {
+      authorWithMostBlogs = {
+        author: blog.author,
+        blogs: frequency[blog.author]
+      }
+    }
+  })
+
+  return authorWithMostBlogs;
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
+
+console.log(mostBlogs([]))
