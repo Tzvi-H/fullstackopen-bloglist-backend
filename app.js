d8340db464ebd040cfd7449d6093683f77ce1308
Tzvi-H <16guitar@gmail.com>
2022-04-08 11:26:10 -0500

    4.18 done

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -21,6 +22,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
